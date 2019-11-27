d5d4bbae589fdd218f7874c1a9d396cc38279c87
zvladyuha <z.vladyuha@yandex.ru>
2019-11-28 02:28:52 +0300

    add 'circle' drawing

--- OpenGL/RenderScene.cpp
+++ OpenGL/RenderScene.cpp
@@ -29,9 +29,63 @@ vector<Vertex> quadVertices{
 
 vector<UINT> quadIndices{0, 1, 2, 2, 1, 3};
 
-UINT uiVBO[4];
-UINT uiVAO[2];
-UINT uiEBO[2];
+vector<Vertex> paintVertices{
+	{ glm::vec3(0.4, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.6, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(0.5, 0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.3, 0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.3, 0.5, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(0.0, 0.4, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.0, 0.6, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(-0.3, 0.5, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(-0.3, 0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(-0.5, 0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(-0.4, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(-0.6, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(-0.3, -0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(-0.5, -0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(-0.3, -0.5, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	
+	{ glm::vec3(0.0, -0.4, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.0, -0.6, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+
+	{ glm::vec3(0.3, -0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.3, -0.5, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	{ glm::vec3(0.5, -0.3, 0.0), glm::vec3(0.0, 0.0, 0.0) },
+	
+};
+
+vector<UINT> paintIndices{
+	0, 1, 2,
+	0, 2, 3,
+	3, 2, 4,
+	3, 4, 5,
+	5, 4, 6,
+	5, 6, 7,
+	5, 7, 8,
+	7, 8, 9,
+	8, 9, 10,
+	10, 9, 11,
+	10, 11, 13,
+	10, 13, 12,
+	12, 13, 14,
+	12, 14, 15,
+	15, 14, 16,
+	15, 16, 18,
+	15, 18, 17,
+	17, 18, 19,
+	17, 19, 0,
+	0, 19, 1
+};
+
+UINT uiVBO[3];
+UINT uiVAO[3];
+UINT uiEBO[3];
 
 CShader shVertex, shFragment;
 CShaderProgram spMain;
@@ -42,9 +96,9 @@ void InitScene(LPVOID lpParam)
 {
 	glClearColor(0.18f, 0.83f, 0.78f, 1.0f);
 
-	glGenVertexArrays(2, uiVAO); // Generate two VAOs, one for triangle and one for quad
-	glGenBuffers(2, uiVBO); // And four VBOs
-	glGenBuffers(2, uiEBO); // Index buffers
+	glGenVertexArrays(3, uiVAO); // Generate two VAOs, one for triangle and one for quad
+	glGenBuffers(3, uiVBO); // And four VBOs
+	glGenBuffers(3, uiEBO); // Index buffers
 
 	// Setup whole triangle
 	glBindVertexArray(uiVAO[0]);
@@ -69,6 +123,21 @@ void InitScene(LPVOID lpParam)
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiEBO[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, quadIndices.size() * sizeof(unsigned int), quadIndices.data(), GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+
+	// Setup whole quad
+	glBindVertexArray(uiVAO[2]);
+
+	glBindBuffer(GL_ARRAY_BUFFER, uiVBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, paintVertices.size() * sizeof(Vertex), paintVertices.data(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiEBO[2]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, paintIndices.size() * sizeof(unsigned int), paintIndices.data(), GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
@@ -104,6 +173,9 @@ void RenderScene(LPVOID lpParam)
 
 	glBindVertexArray(uiVAO[1]);
 	glDrawElements(GL_TRIANGLES, quadIndices.size(), GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(uiVAO[2]);
+	glDrawElements(GL_TRIANGLES, paintIndices.size(), GL_UNSIGNED_INT, 0);
 
 	oglControl->SwapBuffersM();
 }
